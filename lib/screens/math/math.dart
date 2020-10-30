d9efe5df6f2654be28f88083b70f6ea1bf693aa5
gbaranski <gbaranski19@gmail.com>
2020-10-30 10:19:43 +0100

    Add difficulty of each

--- lib/screens/math/math.dart
+++ lib/screens/math/math.dart
@@ -2,37 +2,59 @@ import 'package:allcalculators/screens/math/views/addition.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
+enum MathDifficulty { easy, medium, hard }
+
 class MathView {
   final Widget widget;
   final IconData iconData;
   final String title;
+  final MathDifficulty difficulty;
+
   MathView(
-      {@required this.widget, @required this.iconData, @required this.title});
+      {@required this.widget,
+      @required this.iconData,
+      @required this.title,
+      @required this.difficulty});
 }
 
 class MathScreen extends StatelessWidget {
   final List<MathView> mathViews = [
     MathView(
-        widget: MathAdditionView(),
-        iconData: Icons.add,
-        title: "Basic arithmetic"),
+      widget: MathAdditionView(),
+      iconData: Icons.add,
+      title: "Basic arithmetic",
+      difficulty: MathDifficulty.easy,
+    ),
     MathView(
-        widget: MathAdditionView(),
-        iconData: MdiIcons.function,
-        title: "Functions"),
+      widget: MathAdditionView(),
+      iconData: MdiIcons.rulerSquare,
+      title: "Geometry",
+      difficulty: MathDifficulty.easy,
+    ),
     MathView(
-        widget: MathAdditionView(),
-        iconData: MdiIcons.numeric,
-        title: "Number theory"),
+      widget: MathAdditionView(),
+      iconData: MdiIcons.function,
+      title: "Functions",
+      difficulty: MathDifficulty.medium,
+    ),
     MathView(
-        widget: MathAdditionView(),
-        iconData: MdiIcons.rulerSquare,
-        title: "Geometry"),
+      widget: MathAdditionView(),
+      iconData: MdiIcons.mathIntegral,
+      title: "Integrals",
+      difficulty: MathDifficulty.hard,
+    ),
   ];
 
   Widget singleGroupView(BuildContext context, MathView view) {
     return Card(
         elevation: 0,
+        color: view.difficulty == MathDifficulty.easy
+            ? Colors.green
+            : view.difficulty == MathDifficulty.medium
+                ? Colors.orange
+                : view.difficulty == MathDifficulty.hard
+                    ? Colors.red
+                    : Colors.blueGrey,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         child: InkWell(
           borderRadius: BorderRadius.circular(10),
@@ -44,7 +66,7 @@ class MathScreen extends StatelessWidget {
               children: [
                 Text(
                   view.title,
-                  style: TextStyle(color: Colors.black87, fontSize: 15),
+                  style: TextStyle(color: Colors.black, fontSize: 15),
                 ),
                 Icon(
                   view.iconData,
